073582dd0c1e73fee2e1da85602b4a0797d6558a
sahurul <sahrulk423@gmail.com>
2026-04-23 19:04:22 +0700

    sahrul commit pertama

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return(
         <section id="hero">
-            <h1 className="text-4xl font-bold text-blue-500">Test</h1>
+            <h1 className="text-4xl font-bold text-blue-500">Sahrul</h1>
         </section>
     )
 }
