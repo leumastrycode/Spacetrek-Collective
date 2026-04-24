a13be639bf5c3d64ec86202cd810d07a70462c6f
sahurul <sahrulk423@gmail.com>
2026-04-24 21:45:31 +0700

    about us header percobaan 1

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,6 +1,11 @@
 export default function About() {
     return(
         <section id="about">
+            <div className="flex flex-row  justify-center items-center">
+                <h1 className="font-roboto text-[#E6EAF0] text-48px text-center font-Regular">About </h1> 
+                <h1 className="font-roboto text-[#3D2FFA] text-48px text-center font-Regular ">Us </h1>
+            </div>
+            
 
         </section>
     )
