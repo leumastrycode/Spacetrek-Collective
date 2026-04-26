1305694560dbba94d736dde31b21376eb5a17bff
SukmaPutri <sukmaputri@students.amikom.ac.id>
2026-04-26 13:09:52 +0700

    ubah style

--- components/sections/Reason.tsx
+++ components/sections/Reason.tsx
@@ -1,7 +1,25 @@
 export default function Reason() {
     return(
         <section id="reason">
-            
+            <div>
+            <h1 className="font-bold text-center font-roboto text-[#E6EAF0] text-[48px]">Best
+                <span className="font-bold text-center font-roboto text-[#3D2FFA] text-[48px]"> Reasoned </span>
+            For Choose Our Product</h1>
+            </div>
+            <div>
+            <p className="font-inter font-medium text-center text-[#FFFFFF] text-[26px] leading-[119%] tracking-[4%] align-left align-middle">Explorative Approach</p>
+            <p className="font-inter font-regular text-[22px] text-[#E6EAF0] leading-[119%] tracking-[4%] align-center align-middle">We go beyond the obvious to find unique design directions</p>
+            </div>
+            <br />
+            <div>
+            <p className="font-inter font-medium text-center text-[#FFFFFF] text-[26px] leading-[119%] tracking-[4%] align-left align-middle">Precision in Detail</p>
+            <p>Every element is crafted with intention</p>
+            </div>
+            <div>
+            <p className="font-inter font-medium text-center text-[#FFFFFF] text-[26px] leading-[119%] tracking-[4%] align-left align-middle">Creative Thinking</p>
+            <p className="font-inter font-regular text-[22px] text-[#E6EAF0] leading-[119%] tracking-[4%] align-center align-middle">We turn ideas into strong visual identities</p>
+            </div>
         </section>
+        
     )
 }
