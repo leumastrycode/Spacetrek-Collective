ac362d48f989a144d3b570972fa71ccd0e2851cb
sahurul <sahrulk423@gmail.com>
2026-04-29 20:53:05 +0700

    AboutUs

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -6,39 +6,33 @@ export default function About() {
                     <h1 className="font-roboto text-[#E6EAF0] text-[48px] text-center font-Regular">About </h1> 
                     <h1 className="font-roboto text-[#3D2FFA] text-[48px] text-center font-Regular ">Us </h1>
                 </div>
-                
-                <div className=" flex flex-col gap-[44px] ">
-                    <div className="justify-between">
-                      <svg>
-                        
-                      </svg>
-                    </div>
 
-                     <div  className="max-w-xl flex justify-end items-right">
+                     <div  className="ml-auto w-full flex justify-end text-right">
                     <h1 className="font-inter text-[#E4E4E4] text-normal text-right font-Regular text-[36px]">
                     Navigating brands through the universe of design       
                     </h1>
                     </div>
 
-                    <div className="max-w-xl flex justify-end items-right">
+                    <div className="ml-auto w-full flex justify-end text-right">
                     <p className="font-inter text-[#B0BAC5] text-small text-right font-Regular text-[24px]">
                         SpaceTrek Collective is a creative design team focused on crafting distinctive and forward-thinking brand identities. Inspired by exploration and innovation, we help brands find their direction and translate it into clear, impactful visual designs.
                     </p>
                     </div>
 
-                    <div className="max-w-xl flex justify-end items-right ">
+                    <div className="ml-auto w-full flex justify-end text-right">
                     <p className="font-inter text-[#B0BAC5] text-small text-right font-Regular text-[24px]">
                         Every project is carefully crafted through a collaborative process, ensuring each identity is unique, purposeful, and built to stand out.
                     </p>    
                     </div>
 
-                    <div className="max-w-xl flex justify-end items-right">
+                    <div className="ml-auto w-full flex justify-end text-right">
                     <p className="font-inter text-[#B0BAC5] text-small text-right font-Regular text-[24px]">
                        Every project is carefully crafted through a collaborative process, ensuring each identity is unique, purposeful, and built to stand out.
                     </p>
                     </div>
 
-                </div>
+                
+                
             </div>
             
 
