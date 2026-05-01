7afed040ed779e07a0e0b6cbf742477d52ec210c
samuel <leumastrycode@gmail.com>
2026-05-01 23:35:33 +0700

    Adding Reason section`s background

--- components/sections/Reason.tsx
+++ components/sections/Reason.tsx
@@ -1,6 +1,8 @@
 import FirstReason from "@/components/ui/ReasonItem/FirstReason";
 import SecondReason from "@/components/ui/ReasonItem/SecondReason";
 import ThirdReason from "@/components/ui/ReasonItem/ThirdReason";
+import DotsRectangle from "@/assets-svgr/dots-rectangle3.svg";
+import PatternBlock from "@/assets-svgr/pattern-block.svg";
 
 export default function Reason() {
   return (
@@ -11,10 +13,12 @@ export default function Reason() {
           <span className="text-[#3D2FFA]"> Reasoned </span>
           For Choose <br /> Our Product
         </h2>
-        <div className="flex justify-center  gap-[100px] items-start flex-row px-[30px]">
+        <div className="relative flex justify-center gap-[100px] items-start flex-row px-[30px]">
           <FirstReason />
           <SecondReason />
           <ThirdReason />
+          <DotsRectangle className="absolute -top-[300px] z-[-1]" />
+          <PatternBlock className="absolute -bottom-[500px] z-[-1]" />
         </div>
       </div>
     </section>
