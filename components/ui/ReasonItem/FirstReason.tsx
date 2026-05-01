2484af066355a4fbfd0c4f33aabdfa821dddcfce
samuel <leumastrycode@gmail.com>
2026-05-01 15:10:28 +0700

    Fix the reason section

---
+++ components/ui/ReasonItem/FirstReason.tsx
@@ -0,0 +1,15 @@
+import Reason1 from "@/assets-svgr/asset-reason1.svg";
+
+export default function FirstReason() {
+  return (
+    <div className="flex flex-col w-full max-w-[330px] gap-[20px] justify-center items-center">
+      <Reason1 />
+      <h3 className="font-inter font-medium text-[26px] leading-[119%] tracking-[0.04em] text-center text-[#FFFFFF] mb-[10px]">
+        Explorative Approach
+      </h3>
+      <p className="font-inter font-normal text-[22px] leading-[119%] tracking-[0.04em] text-center text-[#E6EAF0]">
+        We go beyond the obvious to find unique design directions
+      </p>
+    </div>
+  );
+}
