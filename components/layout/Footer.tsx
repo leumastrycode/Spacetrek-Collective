7f607d4d4d7d8777dabba1676882a40b8badeb34
DaffaMashabi24 <daffamashabi24@gmail.com>
2026-05-07 00:55:42 +0700

    Update footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,28 +7,26 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-16">
         
         <div className="flex flex-col">
-          <div className="font-plexMono mb-6">
+          <div className="font-plexMono mb-10">
             <h2 className="leading-none uppercase">
               <span className="text-[32px] tracking-tighter">
                 SPACE<span className="text-[#3D2FFA]">TREK</span>
               </span>
-              <span className="block text-[20px] tracking-[0.2em] mt-1">
+              <span className="block text-[20px] mt-1">
                 COLLECTIVE
               </span>
             </h2>
           </div>
           <p className="font-roboto text-[#B0BAC5] text-[20px] leading-tight mb-12">
-            SpaceTrek Collective is a creative design team 
-            focused on crafting distinctive and forward- 
-            thinking brand identities.
+            SpaceTrek Collective is a creative design team focused on crafting distinctive and forward-thinking brand identities.
           </p>
           <p className="font-roboto text-[#B0BAC5] text-[16px] max-w-[350px]">
             © 2026 SpaceTrek Collective. All rights reserved.
           </p>
         </div>
 
-       <div className="font-roboto flex flex-col space-y-4 text-[18px] md:items-center text-[#B0BAC5]">
-          <div className="flex flex-col space-y-4">
+       <div className="font-roboto flex flex-col space-y-4 text-[24px] md:items-center text-[#B0BAC5]">
+          <div className="flex flex-col space-y-5">
             <Link href="#about" className="hover:text-[#3D2FFA] transition-colors">About Us</Link>
             <Link href="#vision" className="hover:text-[#3D2FFA] transition-colors">Vision & Mission</Link>
             <Link href="#team" className="hover:text-[#3D2FFA] transition-colors">Our Team</Link>
@@ -37,7 +35,7 @@ export default function Footer() {
       </div>
 
         <div className="flex flex-col">
-          <h3 className="font-roboto text-[18px] mb-6 uppercase font-bold">
+          <h3 className="font-roboto text-[24px] mb-6 uppercase font-medium">
             CONTACT <span className="text-[#3D2FFA]">US</span>
           </h3>
           <div className="flex space-x-6">
